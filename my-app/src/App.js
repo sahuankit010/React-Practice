76c5e11d3de31f596efc5a56ad280ef8e556eb98
sahuankit010 <sahu.ankit010@gmail.com>
2023-05-31 19:22:35 -0500

    more practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,59 +1,58 @@
-import './App.css';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [records, setRecords] = useState([]);
+  const [records, setRecords] = useState([])
 
   useEffect(() => {
-    fetchData();
-  }, [])
+    fetchRecords();
+  },)
 
-  async function fetchData() {
+  async function fetchRecords() {
     await fetch(`https://jsonplaceholder.typicode.com/todos`)
-        .then(response => response.json())
-        .then(response => {
-          // response = null
-          setRecords(response);
-          console.log(response);
-  })
-        .catch(err => console.error(err));
-}
-
+      .then((response) => response.json())
+      .then(response => {
+        console.log(response)
+        setRecords(response)
+      })
+      .catch(err => console.error(err))
+  }
 
   return (
-    <div className="App">
-
+    <div>
       {records ? (
-        <table className = "table table-striped" striped bordered hover size="sm" style={{width: '100%'}}>
-        <thead>
-          <tr>
-            <th>
-              ID
-            </th>
-            <th>
-              Title
-            </th>
-            <th>
-              Completed
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-        {Object.keys(records).map((key) =>
-                  <tr>
-                    <td>{records[key].id}</td>
-                    <td>{records[key].title}</td>
-                    <td>{String(records[key].completed)}</td>
-                  </tr>
-                )}
-        </tbody>
-      </table>
-      ) : (<p> Loading...</p>)
-    }
-      
-      
+        <table className="table table-striped" style={{ width: '100%' }}>
+          <thead>
+            <tr>
+              <td>
+                ID
+              </td>
+              <td>
+                Title
+              </td>
+              <td>
+                Completed
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(records).map((key) =>
+              <tr>
+                <td>
+                  {records[key].id}
+                </td>
+                <td>
+                  {records[key].title}
+                </td>
+                <td>
+                  {(String)(records[key].completed)}
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      ) : <p> No data</p>}
     </div>
-  );
+  )
 }
 
 export default App;
